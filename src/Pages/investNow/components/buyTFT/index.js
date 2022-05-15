8e6bce93642b38ee085de947928818753f33d6ca
iBlockchainer <blockchainer.me@gmail.com>
2022-05-16 00:30:59 +0500

    new changes

--- src/Pages/investNow/components/buyTFT/index.js
+++ src/Pages/investNow/components/buyTFT/index.js
@@ -6,9 +6,9 @@ import BuyTftTable from "./components/buyTftTable";
 const BuyTFT = ({ handleBuyAtDiscountClick }) => {
   return (
     <>
-      <h2 className="buyTft-h">
+      <div className="buyTft-h">
         <span>BUY TFT AT AN INSTANT DISCOUNT</span>
-      </h2>
+      </div>
       <div className="buy-tft-wrapper">
         <h4 className="buy-tft-heading">TFT Market Price : $35</h4>
         <BuyTftTable handleBuyAtDiscountClick={handleBuyAtDiscountClick} />
